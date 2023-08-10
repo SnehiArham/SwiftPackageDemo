01303877e07af4c7adb53fbdc696dcd466cc81c1
Snehi_Arham <snehi.shah@arhamtechnosoft.com>
2023-08-10 11:52:55 +0530

    package implement

---
+++ Sources/SwiftPackageExample/SwiftPackageExample.swift
@@ -0,0 +1,10 @@
+public struct SwiftPackageExample {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func HelloWorld() -> String {
+        return "Hello World 123"
+    }
+}
